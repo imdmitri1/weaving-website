6444e38e9e444873d2a40a87e04c4154d9fe4331
D <email.ds@protonmail.com>
2018-02-04 12:53:32 -0600

    reorganize folders

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.get("*", function(req, res){
+  res.redirect("/");
+});
+
+// app.listen(process.env.PORT, process.env.IP);
+app.listen(3000, function(){
+  console.log("running")
+})
